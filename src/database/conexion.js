07c2304cd869d9d842fa0f7768f1a226323a970b
Hollmanick <113219060+Hollmanick@users.noreply.github.com>
2022-12-10 21:59:04 -0500

    Correccion Funciones .env

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
  * Funcion para conectar a la base de datos mongoDB
  */
 const dbConnection = () => {
-    const dbUrl = "mongodb+srv://root:bC3yfGIXuQBAadT2@cluster0.0fqjtje.mongodb.net/alquilerAutos?retryWrites=true&w=majority";
-    //process.env.DB_URI;
+    const dbUrl = process.env.DB_URI;
     mongoose.connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
