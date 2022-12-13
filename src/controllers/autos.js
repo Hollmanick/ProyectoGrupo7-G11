49f9a5b5eaae578561ab921752dc195430d68ab4
stephanye741 <stephanye741@hotmail.com>
2022-12-12 20:30:15 -0500

    cambio de auto

---
+++ src/controllers/autos.js
@@ -0,0 +1,200 @@
+const { Auto } = require("../models/Auto.js")
+const { matchedData } = require("express-validator");
+
+/**
+ * Listar todos los documentos de la coleccion auto
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const getAutos = async (req, res) => {
+    try {
+        // Obtener todos los documentos existentes dentro de la coleccion auto
+        const autos = await Auto.find({}).lean();
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": "documentos existentes dentro de la coleccion auto",
+            "data": autos
+        }).populate("Auto");
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Encontrar un documento de la coleccion auto por _id 
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const getAuto = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("req.params", id);
+        const auto = await Auto.findById(id);
+        console.log(auto);
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": `auto para id: ${id}`,
+            "data": auto
+        }).populate("Auto");
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Recupera el documento de la coleccion auto cuyo id coincida y agrega un campo con los mensajes sus mensaje
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const getMensajesCliente = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("req.params", id);
+        const documento = await Auto.aggregate(
+            [
+                // {   // primera etapa
+                //     $match: {
+                //         "_id": id // donde el campo id coincida
+                //     } 
+                // },
+                {   // segunda etapa
+                    $lookup: {
+                        from: "mensajes", // nombre del schema o coleccion foranea
+                        localField: "_id", // clave del documento local 
+                        foreignField: "auto_id", // clave del documento foraneo
+                        as: "mensajesAuto" // nombre del campo a agregar
+                    }
+                }
+            ]
+        )
+        console.log("getMensajesCliente documento", documento)
+        console.log(documento);
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": `Documento id: ${id} con sus mensajes`,
+            "data": documento
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(200).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Recupera el documento de la coleccion auto cuyo id coincida y agrega un campo con los mensajes sus mensaje
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const getAlquilerCliente = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("req.params", id);
+        const documento = await Auto.aggregate(
+            [
+                {   // primera etapa
+                    $match: {
+                        _id: id // donde el campo id coincida
+                    }
+                },
+                {   // segunda etapa
+                    $lookup: {
+                        from: "Auto", // nombre del schema o coleccion foranea
+                        localField: "_id", // clave del documento local 
+                        foreingField: "auto_id", // clave del documento foraneo
+                        as: "autoAuto" // nombre del campo a agregar
+                    }
+                }
+            ]
+        )
+        console.log("getAutoCliente documento", documento)
+        console.log(documento);
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": `Documento id: ${id} con sus auto`,
+            "data": documento
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(200).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Crear un nuevo documento en la coleccion auto
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const postAuto = async (req, res) => {
+    try {
+        const body = matchedData(req);
+        console.log("req.body", body);
+        const newAuto = await Auto.create(body);
+        console.log(newAuto);
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": "Documento Auto creado"
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(200).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Modificar un documento de la coleccion auto
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const putAuto = async (req, res) => {
+    try {
+        const data = matchedData(req);
+        console.log("req", data.params, data.body);
+        const { id } = req.params;
+        console.log("req.params", id);
+        const documento = await Auto.findByIdAndUpdate(id, data, { new: true });
+        console.log("putAuto documento", documento)
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": `Documento id: ${id} actualizado`,
+            "data": documento
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({ "res_description": error.message })
+    }
+}
+
+/**
+ * Modificar un documento de la coleccion auto
+ * @param {Request} req - Objeto que contiene propiedades de la peticion
+ * @param {Response} res - Objeto que contiene propiedades de la respuesta
+ */
+const deleteAuto = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("req.params", id);
+        const documento = await Auto.findByIdAndDelete(id)
+        console.log("deleteAuto documento", documento)
+        console.log(documento);
+        res.status(200).json({
+            "code_response": 200,
+            "res_description": `Documento id: ${id} eliminado`,
+            "data": documento
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(200).json({ "res_description": error.message })
+    }
+}
+
+module.exports = {
+    getAutos,
+    getAuto,
+    getMensajesCliente,
+    getAlquilerCliente,
+    postAuto,
+    putAuto,
+    deleteAuto
+}
---
+++ src/validators/autos.js
@@ -0,0 +1,16 @@
+const { check, validationResult } = require("express-validator");
+
+const validarBodyAuto = [
+    check("nombre").exists().notEmpty().isLength({ min: 3, max: 50 }),
+    check("marca").exists().notEmpty().isLength({ min: 3, max: 50 }),
+    check("año").exists().notEmpty().isInt().not(),
+    check("descripcion").exists().notEmpty().isLength({ min: 3, max: 50 }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        req.validationResult = true;
+        next();
+    }
+]
+
+module.exports = { validarBodyAuto };
