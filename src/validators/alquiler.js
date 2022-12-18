5d780341b9bd5b3f824d33b5a20c2e3db317e686
Hollmanick <113219060+Hollmanick@users.noreply.github.com>
2022-12-17 20:05:35 -0500

    Modificacion Validador

--- src/validators/alquiler.js
+++ src/validators/alquiler.js
@@ -4,8 +4,8 @@ const validarBodyAlquiler = [
     check("fechaEntrega").exists().notEmpty().isISO8601().toDate().withMessage("Formato fecha ingresada es incorrecto, Ejem: 2022-09-09T00:00:00"),
     check("fechaDevolucion").exists().notEmpty().isISO8601().toDate().withMessage("Formato fecha ingresada es incorrecto, Ejem: 2022-09-09T00:00:00"),
     check("estatus").exists().notEmpty().isString(),
-    check("auto_id").exists().notEmpty().isMongoId(),
-    check("cliente_id").exists().notEmpty().isMongoId(),
+    check("auto_id").isMongoId().optional({ checkFalsy: true }),
+    check("cliente_id").isMongoId().optional({ checkFalsy: true }),
     check("score_id").isMongoId().optional({ checkFalsy: true }),
     (req, res, next) => {
         const errors = validationResult(req);
